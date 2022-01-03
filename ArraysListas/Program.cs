1f97b38f8cfb6815a6ea6daf062e864bf67dd71b
GlobalEmanuel <global.emanuel@ao3tech.com>
2022-01-03 18:20:33 -0300

    Adicionar arquivos de projeto.

---
+++ ArraysListas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArraysListas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Arrays
+            // Exemplo
+            var numbers = new int [] { 1, 2, 3 };
+            var numberCopy = new int[3];
+            // 0-based
+            for(var i = 0; i < numbers.Length; i++)
+            {
+                numberCopy[i] = numbers[i];
+            }
+
+            var numbersString = "0 1 2 3";
+            var numbersArray = numbersString.Split(' ');
+            var numbersConvertedFromString = Array.ConvertAll(numbersArray, Convert.ToInt32);
+        }
+    }
+}
